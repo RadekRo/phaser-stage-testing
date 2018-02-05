1e7c4a8e5d80ffb78b47a3f71840f9d63970fcba
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-05 10:02:01 +0100

    Added highscore list of 5 best results with random names

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const game = new Phaser.Game(640, 480, Phaser.AUTO, 'gameDiv');
 
+window.localStorage.clear();
+
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
