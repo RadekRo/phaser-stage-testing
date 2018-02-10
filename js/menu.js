915c88999f18921bbd8c3145cca7859441f2f55e
Radosław Rocławski <r.roclawski@ankara.pl>
2018-02-10 22:32:02 +0100

    Created template for game screens

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,13 @@ const menuState = {
 
     const nameLabel = game.add.text(30, 80, 'Phaser Stage Testing Game',
                                     { font: '30px Arial', fill: '#ffffff' });
-    const startLabel = game.add.text(30, game.world.height-80, 'press the "W" key to start',
-                                    { font: '25px Arial', fill: '#ffffff' });
-    let wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    // const startLabel = game.add.text(30, game.world.height-80, 'press the "W" key to start',
+     //                               { font: '25px Arial', fill: '#ffffff' });
+    // let wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
-    wkey.onDown.addOnce(this.start, this);
+    const buttonStart = game.add.button(game.world.centerX - 95, 400, 'buttonStart', this.start, this, 2, 1, 0);
+
+    // wkey.onDown.addOnce(this.start, this);
     },
 
     start: function() {
--- js/win.js
+++ js/win.js
@@ -5,12 +5,12 @@ const winState = {
         let currentScore = (this.game.time.totalElapsedSeconds()).toFixed(2);
         const names = ['Radek', 'Jarek', 'Kamila', 'Ania', 'Stefan'];
         const randomName = names[Math.floor(Math.random() * names.length)];
-
-        console.log(randomName);
-
         let highScore = localStorage.getItem('highscore-DWAMT-game');
+
         if (highScore === null) {
+
             highScore = [];
+
         } else {
 
             highScore = JSON.parse(highScore);
@@ -23,26 +23,27 @@ const winState = {
         highScore.sort(function(a, b) { return a.score - b.score});
         highScore = highScore.slice(0,5);
 
-
         const winLabel = game.add.text(30, 80, 'YOU WON! time: ' + currentScore,
                                         { font: '30px Arial', fill: '#00FF00' });
         const highLabel = game.add.text(30, 150, 'Highscores: ',
             { font: '30px Arial', fill: '#00FF00' });
 
-        let elementYPosition  = 165;
-
+        let playerResultYPosition  = 165;
         for (let i = 0; i < highScore.length; i++) {
 
             let listPosition = i+1;
-            elementYPosition = elementYPosition+30;
+            playerResultYPosition = playerResultYPosition+30;
             const firstPlayer = game.add.text
-                                (30, elementYPosition, listPosition + '. ' + highScore[i].name + ': ' + highScore[i].score,
+                                (30, playerResultYPosition, listPosition + '. '
+                                    + highScore[i].name + ': ' + highScore[i].score,
                                 { font: '20px Arial', fill: '#00FF00' });
         }
-        const startLabel = game.add.text(30, game.world.height-80, 'Press the "W" key to restart',
-                                        { font: '20px Arial', fill: '#00FF00' });
-        let wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-        wkey.onDown.addOnce(this.restart, this);
+        // const startLabel = game.add.text(30, game.world.height-80, 'Press the "W" key to restart',
+        //                                { font: '20px Arial', fill: '#00FF00' });
+        const buttonRestart = game.add.button(game.world.centerX - 95, 400, 'buttonRestart', this.restart, this, 2, 1, 0);
+
+        // let wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        // wkey.onDown.addOnce(this.restart, this);
     },
 
     restart: function() {
